4af2baa10382613d2e285246bd3a9d2d1e03e6c2
Aditya Mishra <aditya.iiitsp@gmail.com>
2025-04-10 00:12:08 +0530

    setup login page structure

--- react-app/src/pages/Login.jsx
+++ react-app/src/pages/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="container">
+        
+      <div className="hero-container">
+          <div className="login-container">
+            <div className="icon"></div>
+            <div className="login"></div>
+          </div>
+      </div>
 
-export default Login
+      <div className="footer-container">
+        <div className="footer-items">
+          <div className="item1">Help Center</div>
+          <div className="item2">Terms Of Service</div>
+          <div className="item3">Privacy Policy</div>
+          <div className="item4">@2025EmployWise</div>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Login;
